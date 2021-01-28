c6ad834ee7a0b9638c4ff31ec7b919cb913f6327
Shiba <38932286+KaitakuShiba@users.noreply.github.com>
2021-01-29 01:27:26 +0900

    add-register-fitbit-account

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def render_register_html():
+    return render_template( 'index.html')
