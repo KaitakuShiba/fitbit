f8c3960449ebf7f19bf1b5f2904a4514e0f315d7
Shiba <38932286+KaitakuShiba@users.noreply.github.com>
2021-02-19 07:35:48 +0900

    add-log-dir

--- config/gunicorn_settings.py
+++ config/gunicorn_settings.py
@@ -4,6 +4,6 @@ reload = os.getenv('RELOAD')
 debug = os.getenv('DEBUG')
 bind = '127.0.0.1:5000'
 # if want to print stdout: '-'
-accesslog = os.getenv('LOG')
-errorlog = os.getenv('LOG')
+accesslog = os.getenv('ACCESS_LOG')
+errorlog = os.getenv('ERROR_LOG')
 loglevel = os.getenv('LOG_LEVEL')
